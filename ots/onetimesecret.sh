bb3354755f2553bcdbf35f5696727fc8ca4addef
ash-ishh <mr.akc@outlook.com>
2019-08-19 01:28:59 +0530

    redis migrated to ots

---
+++ ots/onetimesecret.sh
@@ -0,0 +1,19 @@
+echo $(whoami)
+
+echo 'Configure dnsname from $DNSNAME env'
+dnsname=${DNSNAME:-'localhost:7143'}
+sed "s/:host:.*/:host: $dnsname/" -i /etc/onetime/config
+
+echo 'Configure password from $PASSWORD env'
+password=${PASSWORD:-'password'}
+sed "s/# requirepass CHANGEME/requirepass $password/" -i /etc/onetime/redis.conf
+sed "s/redis:\/\/user:CHANGEME@/redis:\/\/user:$password@/" -i /etc/onetime/config
+
+echo 'Configure secret from $SECRET env'
+secret=${SECRET:-'secret'}
+sed "s/:secret:.*/:secret: $password/" -i /etc/onetime/config
+
+echo "Starting up redis..."
+redis-server /etc/onetime/redis.conf
+echo "Starting up onetimesecret..."
+bundle exec thin -e dev -R config.ru -p 7143 start
